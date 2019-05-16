afcc667f3bbd487c5d852c5f406b44a9104cead2
Hieu Tran <trananhhieu07@gmail.com>
2019-05-16 11:58:53 -0400

    push to master.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Intro from './Intro.js';
+import Projects from './Projects.js';
+import Skills from './Skills.js';
+import Contact from './Contact.js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// function App() {
+//   return (
+//     <Intro />,
+//     <Projects />
+//     // <Skills />,
+//     // <Contact />
+//   );
+// }
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+          <Intro />
+          <Projects />
+          <Skills />
+          <Contact />
+      </div>
+    );
+  }
 }
 
 export default App;
